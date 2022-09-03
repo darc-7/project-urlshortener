8a034a87e4ba554a88f0ad61e1036e98f5e591bf
Daniel Ramos <darc7zed@gmail.com>
2022-09-03 18:03:38 -0400

    started url shortener challenge fcc

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -14,11 +16,22 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.post('/api/shorturl', (req,res)=>{
+  let url = req.body.url;
+  const regex = '/^https:/';
+  console.log(url);
+  if(url.toString().match(regex) === null){
+    return res.json({error:'invalid url'});
+  }
+  return res.json({ original_url: url, short_url: 1});
+})
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
